813ae164b0d27c4850127f3b53dcd29b9365e4d8
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-11-05 13:17:02 +0530

    commit 6

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -4,8 +4,8 @@ import React from 'react'
 export default function MainPage() {
     return (
         <div>
-            <h1>Convert Your Currencies</h1>
-            <p>"The Currency Converter web application is your go-to tool for quick and accurate currency conversion. Whether you're a world traveler, international business professional, or simply curious about exchange rates, this user-friendly application simplifies the process of converting one currency to another."</p>
+            <h1 className='lg:mx-32 text-5xl font-bold text-green-600' >Convert Your Currencies</h1>
+            <p className='lg:mx-32 text-m opacity-40 py-8'>"The Currency Converter web application is your go-to tool for quick and accurate currency conversion. Whether you're a world traveler, international business professional, or simply curious about exchange rates, this user-friendly application simplifies the process of converting one currency to another."</p>
         </div>
     )
 }
